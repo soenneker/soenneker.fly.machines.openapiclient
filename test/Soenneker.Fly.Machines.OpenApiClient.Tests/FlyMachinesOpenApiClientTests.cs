790d1d89d0f2be2d28cd91fe518d91c4a6ef112e
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:03:12 -0500

    TUnit conversion

--- test/Soenneker.Fly.Machines.OpenApiClient.Tests/FlyMachinesOpenApiClientTests.cs
+++ test/Soenneker.Fly.Machines.OpenApiClient.Tests/FlyMachinesOpenApiClientTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Fly.Machines.OpenApiClient.Tests;
 
-[Collection("Collection")]
-public sealed class FlyMachinesOpenApiClientTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class FlyMachinesOpenApiClientTests : HostedUnitTest
 {
-    public FlyMachinesOpenApiClientTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public FlyMachinesOpenApiClientTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
